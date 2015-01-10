ab189787397644a14ac54b08a18909b206cf1285
garicchi <xgaricchi@gmail.com>
2015-01-10 23:58:18 +0900

    Add Project

---
+++ RemoteWinPad/RemoteWinPadServer/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RemoteWinPadServer
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        struct POINT
+        {
+            public int X;
+            public int Y;
+
+        }
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            POINT pos = new POINT();
+            
+            GetCursorPos(out pos);
+            SetCursorPos(100,100);
+        }
+    }
+}
