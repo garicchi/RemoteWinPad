76e3d29fa034264e517ebf65ee1c1b983330743f
garicchi <xgaricchi@gmail.com>
2015-01-11 03:35:09 +0900

    sabun

---
+++ RemoteWinPad/RemoteWinPadLib/Data/PadData.cs
@@ -0,0 +1,19 @@
+﻿using RemoteWinPadLib.Type;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteWinPadLib.Data
+{
+    [DataContract]
+    public class PadData
+    {
+        [DataMember]
+        public PadDataType PadDataType { get; set; }
+        [DataMember]
+        public TouchData TouchData { get; set; }
+    }
+}
---
+++ RemoteWinPad/RemoteWinPadLib/Data/TouchData.cs
@@ -0,0 +1,27 @@
+﻿using RemoteWinPadLib.Type;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteWinPadLib.Data
+{
+    [DataContract]
+    public class TouchData
+    {
+        [DataMember]
+        public TouchDataType TouchDataType { get;set;}
+        [DataMember]
+        public int X_Delta { get; set; }
+        [DataMember]
+        public int Y_Delta { get; set; }
+
+        [DataMember]
+        public TouchData()
+        {
+
+        }
+    }
+}
