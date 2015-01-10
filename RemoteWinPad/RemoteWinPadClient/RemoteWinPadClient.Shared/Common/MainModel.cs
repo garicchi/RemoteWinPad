824fc84ef68e034f09b62084940408f625e190f8
garicchi <xgaricchi@gmail.com>
2015-01-11 00:09:31 +0900

    MainModel追加

---
+++ RemoteWinPad/RemoteWinPadClient/RemoteWinPadClient.Shared/Common/MainModel.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemoteWinPadClient.Common
+{
+    public class MainModel:ObservableObject
+    {
+        public MainModel()
+        {
+        }
+    }
+}
